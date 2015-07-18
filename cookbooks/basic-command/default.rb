9ead901bc7a00b194e38052ad33eeb5f571b35ee
tatsuyano <tatsuyano@gmail.com>
2015-07-18 17:01:48 +0900

    Change command yum list

--- cookbooks/basic-command/default.rb
+++ cookbooks/basic-command/default.rb
@@ -16,7 +16,7 @@ end
   execute "Install #{p}" do
     user "root"
     command <<-EOF
-      yum list | grep #{p}
+      yum list | grep #{p}.x86_64
       if [ "$?" -eq 1 ]; then
         rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
       fi
@@ -29,7 +29,7 @@ end
 execute "Install nkf" do
   user "root"
   command <<-EOF
-    yum list | grep nkf
+    yum list | grep nkf.x86_64
     if [ "$?" -eq 1 ]; then
       rpm -ivh http://mirror.centos.org/centos/6/os/x86_64/Packages/nkf-2.0.8b-6.2.el6.x86_64.rpm
     fi
