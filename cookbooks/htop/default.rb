6003798cf72e4564a4df7efb1540eeda269ca1cc
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2015-07-21 14:47:40 +0900

    Add htop

---
+++ cookbooks/htop/default.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+execute "Install htop" do
+  user "root"
+  command <<-EOF
+    yum list | grep htop.x86_64
+    if [ "$?" -eq 1 ]; then
+      rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+      yum --enablerepo=epel install -y htop
+    else
+      yum -y install htop
+   fi
+ 
+  EOF
+  not_if "test -e /usr/bin/htop"
+end
--- roles/centos6.rb
+++ roles/centos6.rb
@@ -1,4 +1,6 @@
-# roles/vagrant
+# $ itamae ssh -h [host_name] -y node.yaml roles/centos6.rb
+
+# Target OS is Centos6 or Amazon Linux.
 
 include_recipe "../cookbooks/basic-command/default.rb"
 include_recipe "../cookbooks/emacs/default.rb"
@@ -7,3 +9,4 @@ include_recipe "../cookbooks/golang/directory_layout_and_ghq.rb"
 include_recipe "../cookbooks/zsh/default.rb"
 include_recipe "../cookbooks/zsh/oh-my-zsh.rb"
 include_recipe "../cookbooks/peco/default.rb"
+include_recipe "../cookbooks/htop/default.rb"
