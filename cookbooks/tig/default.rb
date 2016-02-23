f22317daeb809ab5c717aae2439b2ab7cd591f23
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2016-02-23 19:00:21 +0900

    Update cookbook of tig

--- cookbooks/tig/default.rb
+++ cookbooks/tig/default.rb
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 
 execute "Install tig" do
-  user node[:user]
-  command <<-EOF
-    export GOPATH=/home/#{node[:user]}
-    export PATH=$GOPATH/bin:$PATH
-    ghq get https://github.com/jonas/tig.git
-    cd src/github.com/jonas/tig/
-    make all prefix=/usr/local 
-    sudo make install prefix=/usr/local
-  EOF
-  not_if "test -e /usr/local/bin/tig"
+  user "root"
+  command "yum -y install tig"
 end
 
-# $HOME/.tigrc
-remote_file "/home/#{node[:user]}/.tigrc" do
-  owner node[:user]
-  group node[:user]
-  source "templates/tigrc"
-end
+## $HOME/.tigrc
+#remote_file "/home/#{node[:user]}/.tigrc" do
+#  owner node[:user]
+#  group node[:user]
+#  source "templates/tigrc"
+#end
